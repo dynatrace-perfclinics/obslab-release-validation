83a974fe52b0a58f2570b7771d6e34aefdc92a94
Adam Gardner <adam.gardner@dynatrace.com>
2024-08-02 14:22:57 +1000

    update runtimeChange.sh

--- runtimeChange.sh
+++ runtimeChange.sh
@@ -27,9 +27,16 @@ curl -X POST "$DT_ENDPOINT_UC14/api/v2/events/ingest" \
 ##############
 # Step 2
 # Change the $1 feature flag key to the value of $2
-# Save the resulting JSON back to the same file using sponge
+# This is messy (and in reality would be handled with a proper GitOps process eg. Pull Requests and a tool like ArgoCD)
+# 1. Read flags.yaml with yq
+# 2. Get the JSON flag values from .data and pass to jq
+# 3. jq changes the defaultValue
+# 4. This is saved as an environment variable RESULT
+# 5. Add a pipe and newline to result and use yq to edit flags.yaml inplace
+# 6. kubectl apply the changed file
 ##############
-jq ".flags.$1.defaultVariant = \"$2\"" src/flagd/demo.flagd.json | sponge src/flagd/demo.flagd.json
+RESULT=$(yq '.data["demo.flagd.json"]' .devcontainer/otel-demo/flags.yaml | jq ".flags.$1.defaultVariant = \"$2\"")
+RESULT=$(echo "|\n  $RESULT") yq -i '.data["demo.flagd.json"] = env(RESULT)' .devcontainer/otel-demo/flags.yaml
+kubectl apply -f .devcontainer/otel-demo/flags.yaml
 
-echo
 echo "flag: $1 property: defaultVariant set to value: $2 revert value (in case of problems): $3"
